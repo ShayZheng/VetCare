56b2aaddd1dfaf427ce87ebbc279d31d96157972
ShayZheng <Shayzheng1@gmail.com>
2021-10-05 01:20:16 +0800

    Adding interactive data with two table

---
+++ A1Final/Controllers/InteractiveTableController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using A1Final.Models;
+
+namespace A1Final.Controllers
+{
+    public class InteractiveTableController : Controller
+    {
+        // GET: InteractiveTable
+        public ActionResult Index()
+        {
+            Entities en = new Entities();
+            List<Booking> bookingnames = en.BookingSet.ToList();
+            List<AspNetUsers> users = en.AspNetUsers.ToList();
+
+            var interactivetable = from b in bookingnames
+                                   join u in users on b.AspNetUsersId equals u.Id into table1
+                                   from u in table1.DefaultIfEmpty()
+                                   select new InteractiveTable { bookingdetails = b, userdetails = u };
+
+            return View(interactivetable);
+        }
+    }
+}
---
+++ A1Final/Models/InteractiveTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A1Final.Models
+{
+    public class InteractiveTable
+    {
+        public Booking bookingdetails { get; set; }
+        public AspNetUsers userdetails { get; set; }
+    }
+}
