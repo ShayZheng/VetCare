f6ca12a07945832e3d4de9c889fa8718f557ce55
ShayZheng <Shayzheng1@gmail.com>
2021-10-23 22:26:02 +0800

    Version 10: add bulk email with attachment by using file uploader

--- A1Final/Controllers/HomeController.cs
+++ A1Final/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using A1Final.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace A1Final.Controllers
     [RequireHttps]
     public class HomeController : Controller
     {
+        Entities en = new Entities();
         public ActionResult Index()
         {
             return View();
@@ -49,7 +51,7 @@ namespace A1Final.Controllers
 
                     var client = new SmtpClient("smtp.mailtrap.io", 2525)
                     {
-                        Credentials = new NetworkCredential("2ad108d5eaa64b", "a77e262be93e25"),
+                        Credentials = new NetworkCredential("d671714f565af6", "c83da9acc600f6"),
                         EnableSsl = true
                     };
 
@@ -81,6 +83,80 @@ namespace A1Final.Controllers
                     ModelState.Clear();
 
                     return View(new Models.SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+
+        public ActionResult SendBulkEmail()
+        {
+            return View(new Models.BulkEmailViewModel());
+        }
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult SendBulkEmail(Models.BulkEmailViewModel model, HttpPostedFileBase fileUploader)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    
+
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+
+                    List<AspNetUsers> users = en.AspNetUsers.ToList();
+                    //Get all the user email in database
+                    List<string> emails = (from u in users select u.Email).ToList();
+                    
+
+                    var client = new SmtpClient("smtp.mailtrap.io", 2525)
+                    {
+                        Credentials = new NetworkCredential("d671714f565af6", "c83da9acc600f6"),
+                        EnableSsl = true
+                    };
+
+                    var plainTextContent = contents;
+
+                    //Add a mailmessage which includes from,subject and body.
+                    var mailMessage = new MailMessage
+                    {
+                        From = new MailAddress("from@example.com"),
+                        Subject = subject,
+                        Body = contents,
+                        IsBodyHtml = true,
+                    };
+                    // Add attachment into mailMessage
+                    if (fileUploader != null)
+                    {
+                        string fileName = Path.GetFileName(fileUploader.FileName);
+                        var attachment = new Attachment(fileUploader.InputStream, fileName);
+                        mailMessage.Attachments.Add(attachment);
+                    }
+                    foreach (string userEmail in emails)
+                    {
+                        mailMessage.To.Add(userEmail);
+                        client.Send(mailMessage);
+
+                    };
+                       
+                    
+
+                    //es.Send(toEmail, subject, contents, fileUploader);
+
+                    ViewBag.Result = "Email has been send.";
+
+                    ModelState.Clear();
+
+                    return View(new Models.BulkEmailViewModel());
                 }
                 catch
                 {
---
+++ A1Final/Models/BulkEmailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace A1Final.Models
+{
+    public class BulkEmailViewModel
+    {
+        
+        [Required(ErrorMessage = "Please enter a subject.")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please enter the contents")]
+        public string Contents { get; set; }
+    }
+}
